10f35d2c2554fac02192c7af039369e8191c69be
Tolga Sevencan <tolga.sevencan@wildarmaturen.ch>
2024-02-09 09:43:08 +0100

    new

---
+++ script.js
@@ -0,0 +1,5 @@
+// Hamburger Menü İşlevselliği
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.menu').classList.toggle('active');
+});
