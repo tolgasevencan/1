3e40b2efb63e84ead08ce80b52d5125ce04aecb6
Tolga Sevencan <tolga.sevencan@wildarmaturen.ch>
2024-02-12 14:37:52 +0100

    Weather

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Hamburger Menü İşlevselliği
-document.querySelector('.menu-toggle').addEventListener('click', function() {
+document.querySelector('.menu-toggle').addEventListener('click', function () {
     this.classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
 });
+
+// API'ye istek göndererek hava durumu verilerini alacak fonksiyon
+function getWeather() {
+    const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m';
+
+    // API'ye GET isteği gönderme
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Güncel hava durumu verilerini işleme
+            const currentData = data.current;
+            const temperature = currentData.temperature_2m;
+            const windSpeed = currentData.wind_speed_10m;
+
+            // Hava durumu verilerini HTML içinde gösterme
+            document.getElementById('temperature').textContent = `Sıcaklık: ${temperature}°C`;
+            document.getElementById('windSpeed').textContent = `Rüzgar Hızı: ${windSpeed} m/s`;
+        })
+        .catch(error => {
+            console.error('Hava durumu verisi alınamadı:', error);
+        });
+}
+
+// Sayfa yüklendiğinde hava durumu verilerini al
+window.onload = getWeather;
