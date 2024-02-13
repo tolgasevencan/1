413a105c5a791696d74cd7190cc4740e81fc6716
Tolga Sevencan <tolga.sevencan@wildarmaturen.ch>
2024-02-13 14:46:56 +0100

    Naefels

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('.menu-toggle').addEventListener('click', function () {
 
 // API'ye istek göndererek hava durumu verilerini alacak fonksiyon
 function getWeather() {
-    const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m';
+    const apiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=47.099998&longitude=9.066667&current=temperature_2m,wind_speed_10m';
 
     // API'ye GET isteği gönderme
     fetch(apiUrl)
@@ -18,11 +18,14 @@ function getWeather() {
             const windSpeed = currentData.wind_speed_10m;
 
             // Hava durumu verilerini HTML içinde gösterme
-            document.getElementById('temperature').textContent = `temperature: ${temperature}°C`;
-            document.getElementById('windSpeed').textContent = `windSpeed: ${windSpeed} m/s`;
+            document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
+            document.getElementById('windSpeed').textContent = `Wind Speed: ${windSpeed} m/s`;
+
+            // "Näfels" bilgisini HTML içinde gösterme
+            document.getElementById('location').textContent = 'Näfels';
         })
         .catch(error => {
-            console.error('No weather information available:', error);
+            console.error('No info available:', error);
         });
 }
 
