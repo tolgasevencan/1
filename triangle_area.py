7a5368cd7fa570c8ce84ec2f108a8a0a5f43d5e4
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2025-09-18 15:08:29 +0200

    Create triangle_area.py

---
+++ triangle_area.py
@@ -0,0 +1,7 @@
+# Üçgenin alanını hesaplayan program
+taban = float(input("Üçgenin taban uzunluğunu girin: "))
+yukseklik = float(input("Üçgenin yüksekliğini girin: "))
+
+alan = (taban * yukseklik) / 2
+
+print(f"Üçgenin alanı: {alan}")
